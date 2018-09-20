172eef63d1bd01d1af640f8be5784c9eb4579d2a
superhuman54 <superhuman54@gmail.com>
2018-09-20 14:35:15 +0900

    updated app module

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
+// app.js
 const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res) => res.send('Ok! Hello world!'))
+app.use('/static', (req, res) => {
+    const staticURI = 'https://raw.githubusercontent.com/yoonhoGo/serverless-spa-portfolio/master/static'
+    res.redirect(`${staticURI}${req.path}`)
+})
+
+app.get('/', (req, res) => {
+    res.type('html').sendFile(__dirname + '/index.html')
+})
 
 module.exports = app
